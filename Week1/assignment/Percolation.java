a2f9b58bc3639ae39bc30e92fd081f8d00fd1720
Agrim Prasad <agrimprasad@gmail.com>
2016-12-31 19:13:09 +0800

    Separate out union-find parts to a private sub-class WeightedQuickUnionUF in Percolation.java

--- Week1/assignment/Percolation.java
+++ Week1/assignment/Percolation.java
@@ -1,16 +1,13 @@
 import edu.princeton.cs.algs4.StdOut;
 
 public class Percolation {
- private int edgeLength;   // User specified number of sites per edge of grid
- private int[] id;         // id[i] = component identifier of i
- private int[] sz;         // size of trees, only stores sizes for roots
- private int count;        // number of components
- private boolean[][] openSt;   // 2-D array for open Status of sites
-
- private int numOpen = 0;      // number of open sites
- private int topVirtual;       // Virtual site connected to all elements in the first row (e.g. 0)
- private int botVirtual;       // Virtual site connected to all elements in the last row (e.g. n*n - 1)
-
+ private int edgeLength;          // User specified number of sites per edge of grid
+ private boolean[][] openSt;      // 2-D array for open Status of sites
+ private int topVirtual;          // Virtual site connected to all elements in the first row (e.g. 0)
+ private int botVirtual;          // Virtual site connected to all elements in the last row (e.g. n*n - 1)
+ private WeightedQuickUnionUF uf; // private class for union-find
+ private int numOpen = 0;         // number of open sites
+ 
  public Percolation(int n) {        // initialize by assigning every element to its own component
   if (n <= 0) {
     throw new IllegalArgumentException("Specified edge length " + n + " must be > 0");
@@ -21,40 +18,37 @@ public class Percolation {
   }
 
   int gridSize = n*n;
-
+  uf = new WeightedQuickUnionUF(gridSize);
   edgeLength = n;
-  count = gridSize;
   topVirtual = 0;
   botVirtual = gridSize - 1;
-  id = new int[gridSize];
-  sz = new int[gridSize];
   openSt = new boolean[n][n]; // default false, block all initially
 
-  // int idIdx = 0;
-  for (int i = 0; i < gridSize; i++) { 
-      id[i] = i;
-
-      // if (i == 0) {             // top row
-      //   id[idIdx] = topVirtual;
-      // }
-      // else if (i == n - 1) {    // bottom row
-      //   id[idIdx] = botVirtual;
-      // }
-      // else {
-      //   id[idIdx] = idIdx;
-      // }
-      // idIdx++;
-  }
-
   // top row
-  for (int i = 0; i < n; i++) {
-    union(topVirtual, i);
+  for (int i = 0; i < edgeLength; i++) {
+    uf.union(topVirtual, i);
   }
 
   // bottom row
-  for (int i = n*n - n; i < n; i++) {
-    union(botVirtual, i);
+  for (int i = gridSize - edgeLength; i < gridSize; i++) {
+    uf.union(botVirtual, i);
   }
+
+  // int idIdx = 0;
+  // for (int i = 0; i < gridSize; i++) { 
+  //     id[i] = i;
+
+  //     // if (i == 0) {             // top row
+  //     //   id[idIdx] = topVirtual;
+  //     // }
+  //     // else if (i == n - 1) {    // bottom row
+  //     //   id[idIdx] = botVirtual;
+  //     // }
+  //     // else {
+  //     //   id[idIdx] = idIdx;
+  //     // }
+  //     // idIdx++;
+  // }
  }
 
  // open site (row, col) if it is not open already
@@ -80,25 +74,25 @@ public class Percolation {
         case 0: 
             if (openSt[row][col - 1]) {
               int left1d = convert2dto1d(row, col - 1);
-              union(left1d, site1d);
+              uf.union(left1d, site1d);
             }
             break;
         case 1:
             if (openSt[row - 1][col]) {
               int top1d = convert2dto1d(row - 1, col);
-              union(top1d, site1d);
+              uf.union(top1d, site1d);
             }
             break;               
         case 2:
             if (openSt[row][col + 1]) {
               int right1d = convert2dto1d(row, col + 1);
-              union(right1d, site1d);
+              uf.union(right1d, site1d);
             }
             break;
         case 3:
             if (openSt[row + 1][col]) {
               int bottom1d = convert2dto1d(row + 1, col);
-              union(bottom1d, site1d);
+              uf.union(bottom1d, site1d);
             }
             break;
         default: break;
@@ -127,7 +121,7 @@ public class Percolation {
   validateGridIdx(row);
   validateGridIdx(col);
 
-  return openSt[row][col] && connected(convert2dto1d(row, col), topVirtual);
+  return openSt[row][col] && uf.connected(convert2dto1d(row, col), topVirtual);
  }
 
  // number of open sites
@@ -138,10 +132,10 @@ public class Percolation {
  // does the system percolate?
  public boolean percolates() {
   if (edgeLength == 1) {
-    return openSt[0][0] && connected(topVirtual, botVirtual);
+    return openSt[0][0] && uf.connected(topVirtual, botVirtual);
   }
   else {
-    return connected(topVirtual, botVirtual);
+    return uf.connected(topVirtual, botVirtual);
   }
  }
 
@@ -171,106 +165,119 @@ public class Percolation {
   }
  }
 
- // private class WeightedQuickUnionUF {}
+ private class WeightedQuickUnionUF {
+  private int[] id;         // id[i] = component identifier of i
+  private int[] sz;         // size of trees, only stores sizes for roots
+  private int count;        // number of components
 
- // make sure that p is a valid index for the component id array
- private void validateId(int p) {
-  int n = id.length;
-  if (p < 0 || p >= n) {
-    throw new IndexOutOfBoundsException("index " + p + " is not between 0 and " + (n-1));
-  }
- }
-
- private int root(int p) {
-  while (id[p] != p) {
-    id[p] = id[id[p]]; // Point each node to its grandparent to halve tree length
-    p = id[p];
-  }
-
-  return p;
- }
-
- private void union(int p, int q) {    // add connection between p and q
-  validateId(p);
-  validateId(q);
-
-  int pRoot = root(p);
-  int qRoot = root(q);
-
-  if (pRoot == qRoot) {
-    return;
+  public WeightedQuickUnionUF(int n) {
+    count = n;
+    id = new int[n];
+    sz = new int[n];
+    for (int i = 0; i < n; i++) {
+        id[i] = i;
+        sz[i] = 1;
+    }
   }
 
-  if (sz[pRoot] < sz[qRoot]) {
-    id[pRoot] = qRoot;
-    sz[qRoot] += sz[pRoot];
-  } else {
-    id[qRoot] = pRoot;
-    sz[pRoot] += sz[qRoot];
+  // make sure that p is a valid index for the component id array
+  private void validateId(int p) {
+   int n = id.length;
+   if (p < 0 || p >= n) {
+     throw new IndexOutOfBoundsException("index " + p + " is not between 0 and " + (n-1));
+   }
   }
 
-  count--;
- }
+  private int root(int p) {
+   while (id[p] != p) {
+     id[p] = id[id[p]]; // Point each node to its grandparent to halve tree length
+     p = id[p];
+   }
+   return p;
+  }
 
- private int find(int p) {      // component identifier for p (0 to n-1)
-  validateId(p);
-  return id[p];
- }
+  private void union(int p, int q) {    // add connection between p and q
+   validateId(p);
+   validateId(q);
 
- private boolean connected(int p, int q) {  // return true if p and q are in the same component
-  validateId(p);
-  validateId(q);
-  return root(p) == root(q);
- }
+   int pRoot = root(p);
+   int qRoot = root(q);
 
- private int count() {
-  return count;       // number of components
+   if (pRoot == qRoot) {
+     return;
+   }
+
+   if (sz[pRoot] < sz[qRoot]) {
+     id[pRoot] = qRoot;
+     sz[qRoot] += sz[pRoot];
+   } else {
+     id[qRoot] = pRoot;
+     sz[pRoot] += sz[qRoot];
+   }
+
+   count--;
+  }
+
+  private int find(int p) {      // component identifier for p (0 to n-1)
+   validateId(p);
+   return id[p];
+  }
+
+  private boolean connected(int p, int q) {  // return true if p and q are in the same component
+   validateId(p);
+   validateId(q);
+   return root(p) == root(q);
+  }
+
+  private int count() {
+   return count;       // number of components
+  }
  }
 
  public static void main(String[] args) {
   int n = 4;
-  Percolation uf = new Percolation(n);
+  Percolation perc = new Percolation(n);
 
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= n; j++) {
-      uf.open(i, j);
-      StdOut.println("Open Test: " + uf.isOpen(i, j) + " for n = (" + i + "," + j + ")");
-      StdOut.println("Full Test: " + uf.isFull(i, j) + " for n = (" + i + "," + j + ")");
-      StdOut.println("Percolation Test: " + uf.percolates() + " after n = (" + i + "," + j + ")");
+      perc.open(i, j);
+      StdOut.println("Open Test: " + perc.isOpen(i, j) + " for n = (" + i + "," + j + ")");
+      StdOut.println("Full Test: " + perc.isFull(i, j) + " for n = (" + i + "," + j + ")");
+      StdOut.println("Percolation Test: " + perc.percolates() + " after n = (" + i + "," + j + ")");
     }
   }
-  StdOut.println(uf.numberOfOpenSites() + " total sites open.");
+  StdOut.println(perc.numberOfOpenSites() + " total sites open.");
 
-  Percolation ufMin = new Percolation(n);
-  ufMin.open(1, 1);
-  ufMin.open(2, 2);
-  ufMin.open(3, 2);
-  StdOut.println("Percolation Test before last row site opened: " + ufMin.percolates());
-  ufMin.open(4, 2);
-  StdOut.println("Percolation Test after last row site opened: " + ufMin.percolates());
-  ufMin.open(2, 1);
-  StdOut.println("Percolation Test after connection from top to bottom established: " + ufMin.percolates());
+  Percolation percMin = new Percolation(n);
+  percMin.open(1, 1);
+  percMin.open(2, 2);
+  percMin.open(3, 2);
+  StdOut.println("Percolation Test before last row site opened: " + percMin.percolates());
+  percMin.open(4, 2);
+  StdOut.println("Percolation Test after last row site opened: " + percMin.percolates());
+  percMin.open(2, 1);
+  StdOut.println("Percolation Test after connection from top to bottom established: " + percMin.percolates());
 
   try {
-    ufMin.open(-1, 0);
+    percMin.open(-1, 0);
   }
   catch (IndexOutOfBoundsException e) {
     StdOut.println("Error Caught! " + e.getMessage());
   }
   try {
-    ufMin.isOpen(-1, 0);
+    percMin.isOpen(-1, 0);
   }
   catch (IndexOutOfBoundsException e) {
     StdOut.println("Error Caught! " + e.getMessage());
   }
   try {
-    Percolation ufNeg = new Percolation(-1);
+    Percolation percNeg = new Percolation(-1);
   }
   catch (IllegalArgumentException e) {
     StdOut.println("Error Caught! " + e.getMessage());
   }
   try {
-    Percolation ufNeg = new Percolation(2397*5555);
+    Percolation percBig = new Percolation(2397*5555);
   }
   catch (IllegalArgumentException e) {
     StdOut.println("Error Caught! " + e.getMessage());
