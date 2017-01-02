f0758b63ad82f45751d59c6bb1ee6ac10f8c8e9c
Agrim Prasad <agrimprasad@gmail.com>
2017-01-02 23:30:58 +0800

    Change Permutation to use the randomized queue implementation

--- Week2/assignment/Permutation.java
+++ Week2/assignment/Permutation.java
@@ -1,4 +1,4 @@
-// Permutation: Client for Deque to randomly print items
+// Permutation: Client for RandomizedQueue to randomly print items
 
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
@@ -8,27 +8,14 @@ import com.javamex.classmexer.MemoryUtil;
 
 public class Permutation {
 	public static void main(String[] args) {
-		Deque<String> queue = new Deque<String>();
-		int k = Integer.parseInt(args[0]);;
-		int randomBinary = 0;
+		RandomizedQueue<String> queue = new RandomizedQueue<String>();
+		int k = Integer.parseInt(args[0]);
         while (!StdIn.isEmpty()) {
             String item = StdIn.readString();
-            randomBinary = StdRandom.uniform(0, 2); // 0 or 1
-            if (randomBinary == 0) {
-            	queue.addFirst(item);
-            }
-            else {
-            	queue.addLast(item);
-            }
+            queue.enqueue(item);
         }
         for (int i = 0; i < k; i++) {
-        	randomBinary = StdRandom.uniform(0, 2); // 0 or 1
-        	if (randomBinary == 0) {
-            	StdOut.println(queue.removeFirst());
-            }
-            else {
-            	StdOut.println(queue.removeLast());
-            }
+        	StdOut.println(queue.dequeue());
         }
 	}
 }
