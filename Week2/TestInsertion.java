f73a2f565cb6d0e152681f3a2067c584909f1ac1
Agrim Prasad <agrimprasad@gmail.com>
2017-01-07 13:27:30 +0800

    Add insertion sort with basic test

---
+++ Week2/TestInsertion.java
@@ -0,0 +1,9 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class TestInsertion {
+	public static void main(String[] args) {
+		String[] comparableStr = new String[]{"blah", "a", "sorting", "me"};
+		Selection.sort(comparableStr);
+		Selection.show(comparableStr);
+	}
+}
