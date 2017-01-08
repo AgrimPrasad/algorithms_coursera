ce5c25806d997a8182f9ead853f409d023a1d48a
Agrim Prasad <agrimprasad@gmail.com>
2017-01-08 22:48:43 +0800

    Change point equality method to use the compareTo() method instead of the incorrect equals() method

--- Week3/assignment/BruteCollinearPoints.java
+++ Week3/assignment/BruteCollinearPoints.java
@@ -15,16 +15,16 @@ public class BruteCollinearPoints {
 			if (points[i] == null) throw new NullPointerException();
 			for (int j = i + 1; j < n; j++) {
 				if (points[j] == null) throw new NullPointerException();
-				else if (points[j] == points[i]) throw new IllegalArgumentException("Repeated points found.");
+				else if (areEqual(points, j, i)) throw new IllegalArgumentException("Repeated points found.");
 				for (int k = j + 1; k < n; k++) {
 					if (points[k] == null) throw new NullPointerException();
-					else if (points[k] == points[j]) throw new IllegalArgumentException("Repeated points found.");
+					else if (areEqual(points, k, j)) throw new IllegalArgumentException("Repeated points found.");
 					
 					if (!areCollinear(points, i, j, k)) continue;
 					else {
 						for (int l = k + 1; l < n; l++) {
 							if (points[l] == null) throw new NullPointerException();
-							else if (points[l] == points[k]) throw new IllegalArgumentException("Repeated points found.");
+							else if (areEqual(points, l, k)) throw new IllegalArgumentException("Repeated points found.");
 							
 							if (areCollinear(points, i, j, l)) {
 								segments.add(new LineSegment(points[i], points[l]));
@@ -49,13 +49,18 @@ public class BruteCollinearPoints {
     	return points[i].slopeOrder().compare(points[j], points[k]) == 0;
     }
 
+    // 2 points are equal?
+    private boolean areEqual(Point[] points, int i, int j) {
+    	return points[i].compareTo(points[j]) == 0;
+    }
+
     public static void main(String[] args) {
-    	Point[] pts = new Point[4];
+    	Point[] pts = new Point[5];
     	pts[0] = new Point(0, 0);
     	pts[1] = new Point(1, 1);
-    	pts[3] = new Point(3, 3);
-    	pts[2] = new Point(2, 2);
-    	// pts[3] = new Point(3, 3);
+    	pts[2] = new Point(3, 3);
+    	pts[3] = new Point(2, 2);
+    	pts[4] = new Point(3, 3);
     	BruteCollinearPoints bcPts = new BruteCollinearPoints(pts);
     	StdOut.println("Number of segments: " + bcPts.numberOfSegments());
     	assert bcPts.numberOfSegments() == 1;
